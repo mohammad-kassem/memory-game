b64fd5a66a90c139ca79aa61636da7d32c8a5f1c
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-05-21 20:03:59 +0300

    adding teh main game function and some fixes

--- js/main-script.js
+++ js/main-script.js
@@ -1,6 +1,11 @@
+// game reset and start event
+var start = document.body;
+start.addEventListener("keypress", playGame);
+
 // variable definition
 const color_options = ["green", "red", "yellow", "blue"];
 const pattern = [];
+var pattern_index = 0;
 
 
 // setting the background colors of the game blocks in JS so that they can be accessed and edited easier in JS"
@@ -14,19 +19,27 @@ var game_block_blue = document.getElementById("blue");
 game_block_blue.style.backgroundColor = "blue";
 
 
-//adding the event listners on the event of being clicked for the game block which all use the same callback function userClicked
-game_block_green.addEventListener("click", function(){
-    userClicked(game_block_green);
-}, false);
-game_block_red.addEventListener("click", function(){
-    userClicked(game_block_red);
-}, false);
-game_block_yellow.addEventListener("click", function(){
-    userClicked(game_block_yellow);
-}, false);
-game_block_blue.addEventListener("click", function(){
-    userClicked(game_block_blue);
-}, false);
+//main function
+function playGame(){
+    start.removeEventListener("keypress", playGame);
+    
+    chosen_color = generateColor();
+    //adding the event listners on the event of being clicked for the game block which all use the same callback function userClicked
+    game_block_green.addEventListener("click", function(){
+        userClicked(game_block_green);
+    }, false);
+    game_block_red.addEventListener("click", function(){
+        userClicked(game_block_red);
+    }, false);
+    game_block_yellow.addEventListener("click", function(){
+        userClicked(game_block_yellow);
+    }, false);
+    game_block_blue.addEventListener("click", function(){
+        userClicked(game_block_blue);
+    }, false);
+
+
+}
 
 
 function userClicked(game_block){
@@ -48,13 +61,13 @@ function audioPlay(game_block){
 function changeStyle(game_block, mode){
     var original_bg = game_block.style.backgroundColor;
     console.log(original_bg);
-    if (mode === "active"){
+    if (mode === "click"){
         game_block.style = "background-color: grey; box-shadow: 0 0 10px white;";
     }
     else {
         game_block.style = "opacity: 0";
+
     }
-    console.log(game_block.style);
     setTimeout(function(){
         game_block.style = "";
         game_block.style.backgroundColor = original_bg;
@@ -62,13 +75,13 @@ function changeStyle(game_block, mode){
     
 }
 
+
 function generateColor(){
-    var color_chosen = color_options[Math.floor(Math.random() * color_options.length)];
-    pattern.push(color_chosen);
-    var chosen_game_block = document.getElementById(color_chosen);
+    var chosen_color = color_options[Math.floor(Math.random() * color_options.length)];
+    pattern.push(chosen_color);
+    var chosen_game_block = document.getElementById(chosen_color);
     audioPlay(chosen_game_block);
     changeStyle(chosen_game_block, " ");
+    return chosen_color;
 
 }
-
-chooseGameBlock();
