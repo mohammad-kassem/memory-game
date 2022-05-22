1651e6f1cb15bb72025c3064fda3bc22422e6540
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-05-22 07:32:53 +0300

    implemeneting the functions taking the color as input and start game

--- js/main-script.js
+++ js/main-script.js
@@ -1,13 +1,16 @@
 // game reset and start event
 var start = document.body;
-start.addEventListener("keydown", playGame);
-console.log("hello");
+function startGame(){
+    start.onkeydown = playGame;
+}
 
+startGame();
 
 // variable definition
 const color_options = ["green", "red", "yellow", "blue"];
 var pattern = [];
 var pattern_index = 0;
+var game_over = false;
 
 
 // setting the background colors of the game blocks and body tag in JS so that they can be accessed and edited easier in JS
@@ -25,11 +28,31 @@ game_block_blue.style.backgroundColor = "blue";
 
 //main function
 function playGame(){
-    pattern =[];
-    pattern_index = 0;
-    setTimeout ( ()=>{
-    generateColor();}, 200);
+    start.onkeydown = {};
+    generateColor();
+    turnOnEventListners();
 
+}
+
+
+
+function userClicked(game_block){
+    turnOffEventListners();
+
+    if (game_block.style.backgroundColor === pattern[pattern_index]){
+        pattern_index += 1;
+        var block_color = game_block.style.backgroundColor;
+        audioPlay(block_color);
+        changeStyle(block_color, "click");
+        if (pattern_index === pattern.length){    
+            generateColor();
+            pattern_index = 0;
+    }
+    }
+    else loseGame();
+}
+
+function turnOnEventListners(){
     game_block_green.onclick = () => {userClicked(game_block_green)};
     game_block_red.onclick = () => {userClicked(game_block_red)};
     game_block_yellow.onclick = () => {userClicked(game_block_yellow)};
@@ -38,34 +61,25 @@ function playGame(){
 }
 
 
+function turnOffEventListners(){
+    game_block_green.onclick = () => {};
+    game_block_red.onclick = () => {};
+    game_block_yellow.onclick = () => {};
+    game_block_blue.onclick = () => {};
 
-function userClicked(game_block){
-    if (game_block.style.backgroundColor === pattern[pattern_index]){
-        pattern_index += 1;
-        audioPlay(game_block);
-        changeStyle(game_block, "click");
-        if (pattern_index === pattern.length){
-            setTimeout(function() {
-                generateColor();
-                }, 600);
-            pattern_index = 0;
-    }
-    }
-    else {loseGame();
-        console.log("error");}
 }
 
-function audioPlay(block){
-    var audio_name = block.style.backgroundColor;
-    if (audio_name === "rgb(1, 28, 56)") var audio = new Audio("./assets/sounds/wrong.mp3");
-    else var audio = new Audio(`./assets/sounds/${audio_name}.mp3`);
+
+function audioPlay(block_color){
+    if (block_color === "rgb(1, 28, 56)") var audio = new Audio("./assets/sounds/wrong.mp3");
+    else var audio = new Audio(`./assets/sounds/${block_color}.mp3`);
     audio.play();
 
 }
 
 
-function changeStyle(game_block, mode){
-    var original_background = game_block.style.backgroundColor;
+function changeStyle(block_color, mode){
+    var game_block = document.getElementById(block_color);
     if (mode === "click"){
         game_block.style = "background-color: grey; box-shadow: 0 0 10px white;";
     }
@@ -73,10 +87,10 @@ function changeStyle(game_block, mode){
         game_block.style = "opacity: 0";
 
     }
-    setTimeout(function(){
+    setTimeout(() =>{
         game_block.style = "";
-        game_block.style.backgroundColor = original_background;
-    }, 150);
+        game_block.style.backgroundColor = block_color;
+    }, 250);
     
 }
 
@@ -84,30 +98,29 @@ function generateColor(){
     var chosen_color = color_options[Math.floor(Math.random() * color_options.length)];
     pattern.push(chosen_color);
     console.log(pattern);
-    var chosen_game_block = document.getElementById(chosen_color);
-    audioPlay(chosen_game_block);
-    changeStyle(chosen_game_block, " ");
+    setTimeout(() => {
+        audioPlay(chosen_color);
+        changeStyle(chosen_color, " ");
+
+    }, 600); 
     var status = document.getElementById("heading");
     status.innerText = `Level ${pattern.length}`;
+    turnOnEventListners();
 
 }
 
 function loseGame(){
     var original_background = document.body.style.backgroundColor; 
-    audioPlay(game_body);
+    audioPlay(original_background);
     document.body.style.backgroundColor = "red";
     setTimeout (() => {
         document.body.style.backgroundColor = original_background;
-        console.log("hello");
     }, 200);
     var status = document.getElementById("heading");
     status.innerText = "Game Over, Press Any key To Restart";
     pattern =[];
     pattern_index = 0;
-    game_block_green.onclick = () => {};
-    game_block_red.onclick = () => {};
-    game_block_yellow.onclick = () => {};
-    game_block_blue.onclick = () => {};
-
+    turnOffEventListners;
+    startGame();
 
 }
