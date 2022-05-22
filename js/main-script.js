493dc017e1b675e7a0e8b25b2822e72e8da07331
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-05-22 09:38:18 +0300

    minor changes and fixes in JS

--- js/main-script.js
+++ js/main-script.js
@@ -4,13 +4,10 @@ function startGame(){
     start.onkeydown = playGame;
 }
 
-startGame();
-
 // variable definition
 const color_options = ["green", "red", "yellow", "blue"];
 var pattern = [];
 var pattern_index = 0;
-var game_over = false;
 
 
 // setting the background colors of the game blocks and body tag in JS so that they can be accessed and edited easier in JS
@@ -26,34 +23,8 @@ var game_block_blue = document.getElementById("blue");
 game_block_blue.style.backgroundColor = "blue";
 
 
-//main function
-function playGame(){
-    start.onkeydown = {};
-    generateColor();
-    turnOnEventListners();
-
-}
-
-
-
-function userClicked(game_block){
-    turnOffEventListners();
-
-    if (game_block.style.backgroundColor === pattern[pattern_index]){
-        pattern_index += 1;
-        var block_color = game_block.style.backgroundColor;
-        audioPlay(block_color);
-        changeStyle(block_color, "click");
-        if (pattern_index === pattern.length){    
-            generateColor();
-            pattern_index = 0;
-    }
-    }
-    else loseGame();
-}
-
 function turnOnEventListners(){
-    game_block_green.onclick = () => {userClicked(game_block_green)};
+    game_block_green.onclick = (event) => {userClicked(game_block_green)};
     game_block_red.onclick = () => {userClicked(game_block_red)};
     game_block_yellow.onclick = () => {userClicked(game_block_yellow)};
     game_block_blue.onclick = () => {userClicked(game_block_blue)};
@@ -70,6 +41,37 @@ function turnOffEventListners(){
 }
 
 
+startGame();
+
+//main function
+function playGame(){
+    start.onkeydown = {};
+    generateColor();
+    turnOnEventListners();
+
+}
+
+
+// the click event callback function declaration
+function userClicked(game_block){
+    if (game_block.style.backgroundColor === pattern[pattern_index]){
+        pattern_index += 1;
+        var block_color = game_block.style.backgroundColor;
+        audioPlay(block_color);
+        changeStyle(block_color, "click");
+        if (pattern_index === pattern.length){    
+            generateColor();
+            pattern_index = 0;
+
+        
+        }
+    }
+
+    else loseGame();
+
+}
+
+
 function audioPlay(block_color){
     if (block_color === "rgb(1, 28, 56)") var audio = new Audio("./assets/sounds/wrong.mp3");
     else var audio = new Audio(`./assets/sounds/${block_color}.mp3`);
@@ -77,7 +79,7 @@ function audioPlay(block_color){
 
 }
 
-
+// changes the style of the game-block to highlight when clicked or if it's been added to the pattern
 function changeStyle(block_color, mode){
     var game_block = document.getElementById(block_color);
     if (mode === "click"){
@@ -90,14 +92,15 @@ function changeStyle(block_color, mode){
     setTimeout(() =>{
         game_block.style = "";
         game_block.style.backgroundColor = block_color;
-    }, 250);
+    }, 200);
+    clearTimeout;
     
 }
 
+// generates the next color in the pattern randomly and psuhes it into the pattern array while calling changeStyle to highlight it
 function generateColor(){
     var chosen_color = color_options[Math.floor(Math.random() * color_options.length)];
     pattern.push(chosen_color);
-    console.log(pattern);
     setTimeout(() => {
         audioPlay(chosen_color);
         changeStyle(chosen_color, " ");
@@ -105,22 +108,22 @@ function generateColor(){
     }, 600); 
     var status = document.getElementById("heading");
     status.innerText = `Level ${pattern.length}`;
-    turnOnEventListners();
 
 }
 
+// handles changing the background and then back and playing the sound when the user loses (enters the pattern wrong)
 function loseGame(){
     var original_background = document.body.style.backgroundColor; 
     audioPlay(original_background);
-    document.body.style.backgroundColor = "red";
-    setTimeout (() => {
-        document.body.style.backgroundColor = original_background;
-    }, 200);
     var status = document.getElementById("heading");
     status.innerText = "Game Over, Press Any key To Restart";
     pattern =[];
     pattern_index = 0;
-    turnOffEventListners;
+    turnOffEventListners();
+    document.body.style.backgroundColor = "red";
+    setTimeout (() => {
+        document.body.style.backgroundColor = original_background;
+    }, 200);
     startGame();
 
 }
